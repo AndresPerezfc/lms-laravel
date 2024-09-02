ae75f06fbb0bf947a1ffb612ef09ea38a23067af
andresperezfc <felipe3apc@gmail.com>
2024-09-02 17:08:35 -0500

    Haciendo responsitve el Sidebar

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,38 +1,45 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-        <!-- Styles -->
-        @livewireStyles
+    <!-- Styles -->
+    @livewireStyles
 </head>
-<body>
-      
-      @include('layouts.includes.admin.navigation')
-      @include('layouts.includes.admin.sidebar')
-      
-      <div class="p-4 sm:ml-64">
-         <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
-            
-            {{$slot}}
 
-         </div>
-      </div>
-      
-  
-  @stack('modals')
+<body x-data="{
+    open: false,
+}":class="{'overflow-hidden': open}" class="sm:overflow-auto">
 
-  @livewireScripts
+    @include('layouts.includes.admin.navigation')
+    @include('layouts.includes.admin.sidebar')
+
+    <div class="p-4 sm:ml-64">
+        <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
+
+            {{ $slot }}
+
+        </div>
+    </div>
+
+
+    <div x-cloak x-show="open" x-on:click="open = false" class="bg-gray-900 bg-opacity-50 fixed inset-0 z-30 sm:hidden"></div>
+
+    @stack('modals')
+
+    @livewireScripts
 </body>
+
 </html>
