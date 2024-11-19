cd50d8337824063e2008c2044ac26930a1183dcf
andresperezfc <felipe3apc@gmail.com>
2024-11-18 22:17:58 -0500

    Se agregaron los campos a cada uno de los modelos

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -7,5 +7,41 @@ use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'title',
+        'slug',
+        'summary',
+        'description',
+        'status',
+        'image_path',
+        'video_path',
+        'welcome_message',
+        'good_bye_message',
+        'observation',
+        'user_id',
+        'level_id',
+        'category_id',
+        'price_id',
+    ];
+
+
+    public function teacher()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function level()
+    {
+        return $this->belongsTo(Level::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function price()
+    {
+        return $this->belongsTo(Price::class);
+    }
 }
