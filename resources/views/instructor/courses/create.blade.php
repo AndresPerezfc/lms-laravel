c5d2b8ebceb29b52f7b001193eb1a42dc0aacc62
andresperezfc <felipe3apc@gmail.com>
2024-11-21 22:55:47 -0500

    Se agregó la posibilidad de agregar el ancho al container donde se crearan los cursos, ademas que se llamaron en el controlador los modelos de prices, level y categories

--- resources/views/instructor/courses/create.blade.php
+++ resources/views/instructor/courses/create.blade.php
@@ -1,3 +1,15 @@
 <x-instructor-layout>
 
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Crear curso
+        </h2>
+    </x-slot>
+
+    <x-container class="mt-12" width="4xl">
+        <div class="bg-white rounded-lg shadow-lg p-6">
+
+        </div>
+    </x-container>
+
 </x-instructor-layout>
