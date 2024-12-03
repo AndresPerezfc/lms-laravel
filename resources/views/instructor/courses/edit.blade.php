8f1e20e6c352985162ef6a64e63bf219599b2ca9
andresperezfc <felipe3apc@gmail.com>
2024-12-02 21:46:38 -0500

    Inicio - Creación formulario de edición, agregado el input del title

--- resources/views/instructor/courses/edit.blade.php
+++ resources/views/instructor/courses/edit.blade.php
@@ -28,7 +28,21 @@
 
             <div class="grid-col-span-1 lg:col-span-4">
                 <div class="card">
+                    <form action="{{route('instructor.courses.update', $course)}} method=" POST" enctype="multipart/form-data"">
+                    @csrf
+                    @method('PUT')
 
+                    <p class=" text-2xl font-semibold">Información del curso</p>
+
+                        <hr class="mt-2 mb-6">
+
+                        <x-validation-errors />
+
+                        <div class="mb-4">
+                            <x-label value="Titulo del curso"></x-label>
+                            <x-input class="w-full" value="{{old('tile', $course->title)}}" name="title"></x-input>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
