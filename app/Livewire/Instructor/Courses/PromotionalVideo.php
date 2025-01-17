5d7a05b3cf8501a6715de04dbc44d899fb0f2b65
andresperezfc <felipe3apc@gmail.com>
2025-01-17 16:01:21 -0500

    Agregado reproductor Plyer Js

--- app/Livewire/Instructor/Courses/PromotionalVideo.php
+++ app/Livewire/Instructor/Courses/PromotionalVideo.php
@@ -15,13 +15,15 @@ class PromotionalVideo extends Component
     public $video;
 
     protected $rules = [
-        'video' => 'required|mimes:mp4,mov,avi|max:10240', // Asegúrate de definir los tipos MIME correctos y un tamaño máximo adecuado
+        'video' => 'required|mimes:mp4,mov,avi|max:50240', // Asegúrate de definir los tipos MIME correctos y un tamaño máximo adecuado
     ];
 
     public function save(){
         $this->validate();
         $this->course->video_path = $this->video->store('course/promotional-videos', 'public');
         $this->course->save();
+
+        return $this->redirectRoute('instructor.courses.video', $this->course,true, true);
     }
 
     
--- resources/views/livewire/instructor/courses/promotional-video.blade.php
+++ resources/views/livewire/instructor/courses/promotional-video.blade.php
@@ -1,4 +1,9 @@
 <div>
+
+    @push('css')
+        <link rel="stylesheet" href="https://cdn.plyr.io/3.7.8/plyr.css" />
+    @endpush
+
     <h1 class="text-2xl font-semibold">
         Video Promocional
     </h1>
@@ -8,9 +13,15 @@
     <div class="grid grid-cols-2 gap-6">
         <div class="col-span-1">
             @if ($course->video_path)
-            <video controls class="aspect-video object-cover">
-                <source src="{{Storage::url($course->video_path)}}" type="video/mp4">
-            </video> 
+            <div wire:ignore>
+                <div x-data x-init="
+                let player= new Plyr($refs.player);
+            ">
+                <video x-ref="player" playsinline controls data-poster="{{ $course->image }}" class="aspect-video object-cover" >
+                    <source src="{{Storage::url($course->video_path)}}" type="video/mp4">
+                </video> 
+            </div>
+            </div>
             @else
                 <figure>
                     <img class="w-full aspect-video object-cover" src="{{ $course->image }}" alt="{{ $course->title }}">
@@ -19,6 +30,16 @@
         </div>
         <div class="col-span-1">
             <form wire:submit="save">
+                @if ($errors->any())
+                <div class="mb-4 p-4 bg-red-100 text-red-700 rounded">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
                 <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit fuga tempore quisquam,
                     nisi pariatur
                     consectetur? Illo vitae autem, beatae esse sapiente repudiandae voluptates rerum similique? Quaerat
@@ -35,5 +56,10 @@
 
         </div>
     </div>
+
+    @push('js')
+        <script src="https://cdn.plyr.io/3.7.8/plyr.js"></script>
+        
+    @endpush
 
 </div>
