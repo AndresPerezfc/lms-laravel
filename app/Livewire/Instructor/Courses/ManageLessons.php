fb6ef2b28a2f24648c63c17ccceae64b701db7b6
Andres Pereze <felipe3apc@gmail.com>
2025-03-13 22:10:28 -0500

    Subida de video y creación de lección completada

--- app/Livewire/Instructor/Courses/ManageLessons.php
+++ app/Livewire/Instructor/Courses/ManageLessons.php
@@ -22,6 +22,53 @@ class ManageLessons extends Component
         'video_original_name' => null,
     ];
 
+    public function rules()
+    {
+        $rules = [
+            'lessonCreate.name' => 'required',
+            'lessonCreate.platform' => 'required',
+        ];
+
+        if ($this->lessonCreate['platform'] == 1) {
+            $rules['video'] = 'required|mimes:mp4,mov,avi,wmv,flv,3gp';
+        } else {
+            $rules['url'] = ['required', 'regex:/^(?:https?:\/\/)?(?:www\.)?(youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=))([\w-]{10,12})/'];
+        }
+
+        return $rules;
+    }
+
+    public function store(){
+        $this->validate();
+
+        if($this->lessonCreate['platform'] == 1){
+
+            $videoPath = $this->video->store('courses/lessons');
+            $this->lessonCreate['video_original_name'] = $this->video->getClientOriginalName();
+            $this->lessonCreate['video_path'] = $videoPath; // Aquí aseguramos que no quede en null
+
+            $lesson = $this->section->lessons()->create($this->lessonCreate);
+
+            $this->dispatch('uploadVideo', $lesson->id, video: $this->video)->self();
+
+        }elseif($this->lessonCreate['platform'] == 2){
+            $this->lessonCreate['video_original_name'] = $this->url;
+
+            $lesson = $this->section->lessons()->create($this->lessonCreate);
+        }
+
+        $this->reset(['url', 'lessonCreate']);
+    }
+
+    #[On('uploadVideo')]
+    public function uploadVideo($lessonId){
+
+        $lesson = Lesson::find($lessonId);
+        $lesson->save();
+
+        $this->reset('video'); 
+    }
+
     public function render()
     {
         return view('livewire.instructor.courses.manage-lessons');
