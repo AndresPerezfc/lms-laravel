e44f4f54139147e2cb5bc4d5884526756d8d3a79
Andres Pereze <felipe3apc@gmail.com>
2025-05-13 22:02:56 -0500

    Diseño de pagina de bienvenida

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,62 @@
 <x-app-layout>
-    
+    <figure class="mb-20">
+        <img class="w-full aspect-[3/1] object-cover object-center" src="{{asset('img/welcome/coding-924920_1280.jpg')}}" alt="">
+    </figure>
+
+    <section>
+        <x-container>
+            <h1 class="text-2xl font-semibold text-center mb-8">CONTENIDO</h1>
+
+            <ul class="grid grid-cols-4 gap-6">
+                <li>
+                    <a href="">
+                        <img class="aspect-[16/9] object-cover object-center rounded-lg" src="https://codersfree.com/img/servicios/cursos.jpeg" alt="">
+                    </a>
+
+                    <h1 class="text-xl text-center font-semibold mt-4 mb-2">
+                        <a href="">Cursos Online</a>
+                    </h1>
+
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi, nulla.</p>
+                </li>
+
+                <li>
+                    <a href="">
+                        <img class="aspect-[16/9] object-cover object-center rounded-lg" src="https://codersfree.com/img/servicios/desarrollo.jpeg" alt="">
+                    </a>
+
+                    <h1 class="text-xl text-center font-semibold mt-4 mb-2">
+                        <a href="">Desarrollo de software</a>
+                    </h1>
+
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi, nulla.</p>
+                </li>
+
+                <li>
+                    <a href="">
+                        <img class="aspect-[16/9] object-cover object-center rounded-lg" src="https://codersfree.com/img/servicios/asesorias.jpg" alt="">
+                    </a>
+
+                    <h1 class="text-xl text-center font-semibold mt-4 mb-2">
+                        <a href="">Asesorías Online</a>
+                    </h1>
+
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi, nulla.</p>
+                </li>
+
+                <li>
+                    <a href="">
+                        <img class="aspect-[16/9] object-cover object-center rounded-lg" src="https://codersfree.com/img/servicios/blog.jpeg" alt="">
+                    </a>
+
+                    <h1 class="text-xl text-center font-semibold mt-4 mb-2">
+                        <a href="">Blog</a>
+                    </h1>
+
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi, nulla.</p>
+                </li>
+            </ul>
+        </x-container>
+        
+    </section>
 </x-app-layout>
